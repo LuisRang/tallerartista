1c22c9e8143ea4d20fba105e68c5582996ed8a39
LuisRang <luisfrangel94@gmail.com>
2021-09-21 19:10:18 -0500

    Primer commit

---
+++ JavaScript/1.js
@@ -0,0 +1,35 @@
+const nombreAlbum1 = () => {
+
+    alert("Nombre del allbum: Motor like");
+  
+}
+
+const nombreAlbum2 = () => {
+
+    alert("Nombre del allbum: Home");
+  
+}
+
+const nombreAlbum3 = () => {
+
+    alert("Nombre del allbum: Feleac");
+  
+}
+
+const nombreAlbum4 = () => {
+
+   alert("Nombre del allbum: Motion Manifold");
+  
+}
+
+const nombreAlbum5 = () => {
+
+    alert("Nombre del allbum: Last One");
+  
+}
+
+const nombreAlbum6 = () => {
+
+    alert("Nombre del allbum: Disfuction");
+  
+}
